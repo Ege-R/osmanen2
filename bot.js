afc95d8d9db1d2ec3dd4c2027517f20a9eee9401
Ege-R <38590133+Ege-R@users.noreply.github.com>
2018-04-22 22:23:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,20 +2,39 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+  console.log(`Hop Dayı Çevrimiçiyiz ${client.user.tag}!`);
+});
+client.on('message', msg => {
+  if (msg.content === 'Sa') {
+    msg.reply('Aleyküm Selam Gardaşım');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Nasılsın') {
+    msg.reply('İyidir Gardaş Sen ?');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Hava Durumu') {
+    msg.reply('Git Google dan bak Pezevenk');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pinginizin Değeri => ' + client.ping);
+  }
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('PONG!');
-  	}
+  if (message.content === 'Fotoğrafım') {
+
+    message.reply(message.author.avatarURL);
+  }
 });
 
-client.on('message', message => {
-    if (message.content === 'bing') {
-    	message.reply('BONG!');
-  	}
-});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
